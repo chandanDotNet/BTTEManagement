a4dc94f007a1b318fd730251d3633297768f8741
siddiqui.rahil <siddiqui.rahil@LT-276-HO>
2024-09-06 17:41:01 +0530

    Check

--- POS.Data/Entities/Trip/TripHotelBooking.cs
+++ POS.Data/Entities/Trip/TripHotelBooking.cs
@@ -10,7 +10,6 @@ namespace BTTEM.Data
 {
     public class TripHotelBooking : BaseEntity
     {
-
         public Guid Id { get; set; }
         public Guid TripId { get; set; }
         public string BookTypeBy { get; set; }
@@ -23,7 +22,6 @@ namespace BTTEM.Data
         public decimal? TentativeAmount { get; set; }
         public string? BookStatus { get; set; }
         public Guid? ExpenseId { get; set; }
-
         public string? ApprovalStatus { get; set; }
         public DateTime? ApprovalStatusDate { get; set; }
         public Guid? ApprovalStatusBy { get; set; }
@@ -32,7 +30,6 @@ namespace BTTEM.Data
         public string? BillReceiptPath { get; set; }
         public string? ApprovalDocumentsReceiptName { get; set; }
         public string? ApprovalDocumentsReceiptPath { get; set; }
-
         public string? CityName { get; set; }
         public string? NearbyLocation { get; set; }
         public string? PreferredHotel { get; set; }
