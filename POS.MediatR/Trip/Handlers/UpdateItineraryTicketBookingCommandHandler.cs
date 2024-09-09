6b28251b7337cef053615431b093229ada8cf1ac
siddiqui.rahil <siddiqui.rahil@LT-276-HO>
2024-09-09 12:03:55 +0530

    Calculation

--- POS.MediatR/Trip/Handlers/UpdateItineraryTicketBookingCommandHandler.cs
+++ POS.MediatR/Trip/Handlers/UpdateItineraryTicketBookingCommandHandler.cs
@@ -103,14 +103,14 @@ namespace BTTEM.MediatR.Trip.Handlers
                 entityExist.TotalAmount = (request.AgentCharge == null ? entityExist.AgentCharge : request.AgentCharge)
                                          + (request.BookingAmount == null ? entityExist.BookingAmount : request.BookingAmount)
                                          + (request.CancelationCharge == null ? entityExist.CancelationCharge : request.CancelationCharge)
-                                         + request.RescheduleCharge;
+                                         + (request.RescheduleCharge == null ? entityExist.RescheduleCharge : request.RescheduleCharge);
             }
             else
             {
                 entityExist.TotalAmount = (request.AgentCharge == null ? entityExist.AgentCharge : request.AgentCharge)
                                          + (request.BookingAmount == null ? entityExist.BookingAmount : request.BookingAmount)
                                          + (request.CancelationCharge == null ? entityExist.CancelationCharge : request.CancelationCharge)
-                                         - request.RescheduleCharge;
+                                         - (request.RescheduleCharge == null ? entityExist.RescheduleCharge : request.RescheduleCharge);
             }
             //}
             entityExist.IsAvail = request.IsAvail;
