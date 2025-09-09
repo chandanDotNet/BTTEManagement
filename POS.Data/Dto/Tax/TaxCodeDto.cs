9e0cbf4221518bcd481d4ceae8a142cabe87c03f
chandanDotNet <chandan.mandal@shyamfuture.com>
2025-09-09 17:07:12 +0530

    changes 1

--- POS.Data/Dto/Tax/TaxCodeDto.cs
+++ POS.Data/Dto/Tax/TaxCodeDto.cs
@@ -9,5 +9,10 @@ namespace POS.Data.Dto
     public class TaxCodeDto
     {
         public string Name { get; set; }
+        public string TaxCodeDetails { get; set; }
+        public decimal CGST { get; set; }
+        public decimal SGST { get; set; }
+        public decimal IGST { get; set; }
+        public Boolean IsDeleted { get; set; }
     }
 }
--- POS.Data/Entities/Lookups/TaxCode.cs
+++ POS.Data/Entities/Lookups/TaxCode.cs
@@ -9,5 +9,10 @@ namespace POS.Data
     public class TaxCode
     {
         public string Name { get; set; }
+        public string TaxCodeDetails { get; set; }
+        public decimal CGST { get; set; }
+        public decimal SGST { get; set; }
+        public decimal IGST { get; set; }
+        public Boolean IsDeleted { get; set; }
     }
 }
